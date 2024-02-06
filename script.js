7b1507eceee1316ef45fae0204e46f3555968eb7
SouravDas1220 <104808352+SouravDas1220@users.noreply.github.com>
2024-02-06 11:58:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const url = 'https://foreca-weather.p.rapidapi.com/location/search/mumbai?lang=en&country=in';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'd0001f801dmshba1022e44338100p140c62jsn3c52d0dc719a',
+// 		'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
